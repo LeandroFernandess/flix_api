d54d3ace205e46c7dda0d4b55b4ed89bf4b653a6
Leandro Fernandes <leandrofernandes1600@gmail.com>
2024-12-17 19:23:56 -0300

    first commit

---
+++ movies/serializers.py
@@ -0,0 +1,43 @@
+from django.db.models import Avg
+from rest_framework import serializers
+from movies.models import Movie
+from genres.serializers import GenreSerializer
+from actors.serializers import ActorSerializer
+
+
+class MovieModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError(
+                "A data de lançamento não pode ser anterior a 1990."
+            )
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError(
+                "O tamanho do resumo não pode ser superior a 200 caracteres."
+            )
+        return value
+
+
+class MovieListDetailSerializer(serializers.ModelSerializer):
+
+    actors = ActorSerializer(many=True)
+    genre = GenreSerializer()
+    rate = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Movie
+        fields = ["id", "title", "genre", "actors", "release_date", "rate", "resume"]
+
+    def get_rate(self, object):
+        rate = object.reviews.aggregate(Avg("stars"))["stars__avg"]
+        if rate:
+            return round(rate, 1)
+        return None
